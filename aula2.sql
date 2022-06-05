c42dd7189f4c395ae58c7b1b5b1294519ffb3d87
Alexandro Santos <alexandro.raboni@gmail.com>
2022-06-05 15:04:48 -0300

    Aula finalizada

--- aula2.sql
+++ aula2.sql
@@ -1,3 +1,23 @@
 drop database if exists aula_banco;
 create database aula_banco;
 use aula_banco;
+
+-- Criando tabela:
+create table estado(
+	id int not null auto_increment, -- primary key,
+    nome varchar(200) not null unique, -- constraint inline.
+    sigla char(2) not null unique,
+    ativo char(1) not null default 'S',
+    data_cadastro datetime not null default current_timestamp,
+    constraint pk_estado primary key(id),
+    -- check(ativo in('S', 'N')) -- constraint out of line -- regra sem nome
+    constraint coluna_ativo_deve_ser_S_ou_N check(ativo in('S', 'N')) -- constraint out of line. Regra com nome
+);
+
+-- Inserindo dados na tabela:
+-- insert into estado(id, nome, sigla, ativo, data_cadastro) value(1, 'Paraná', 'PR', 'S', '2022-06-05');
+-- insert into estado(id, nome, sigla, ativo, data_cadastro) value(2, 'São Paulo', 'SP', 'N', '2022-06-05');
+insert into estado(nome, sigla) value('Maranhão', 'MA');
+insert into estado(nome, sigla) value('Goiás', 'GO');
+
+select * from estado;
